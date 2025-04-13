8304cb9bf07b711d50c6e3a32143f99e59e07762
Karuna1630 <karunagiri481@gmail.com>
2025-04-13 06:15:36 +0545

    added DAO and connection to database

--- src/main/java/com/example/librarymanagementsystem/model/Book.java
+++ src/main/java/com/example/librarymanagementsystem/model/Book.java
@@ -1,4 +1,62 @@
 package com.example.librarymanagementsystem.model;
 
 public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String category;
+
+    public Book(int bookId, String title, String author, String publisher, int publicationYear, String category) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.category = category;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    @Override
+    public String toString() {
+        return "Book [bookId=" + bookId + ", title=" + title + ", author=" + author + "]";
+    }
 }
+
---
+++ src/main/java/com/example/librarymanagementsystem/model/Borrowing.java
@@ -0,0 +1,79 @@
+package com.example.librarymanagementsystem.model;
+
+import java.util.Date;
+
+public class Borrowing {
+        private int borrowingId;
+        private int bookId;
+        private int userId;
+        private Date borrowDate;
+        private Date dueDate;
+        private Date returnDate;
+        private String status;
+
+    public Borrowing(int borrowingId, int bookId, int userId, Date borrowDate, Date dueDate, Date returnDate, String status) {
+        this.borrowingId = borrowingId;
+        this.bookId = bookId;
+        this.userId = userId;
+        this.borrowDate = borrowDate;
+        this.dueDate = dueDate;
+        this.returnDate = returnDate;
+        this.status = status;
+    }
+
+    public int getBorrowingId() {
+        return borrowingId;
+    }
+
+    public void setBorrowingId(int borrowingId) {
+        this.borrowingId = borrowingId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Date getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(Date borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
