ac2e6f1502bea69bd97fd1c9c5719334f75a7058
Karuna1630 <karunagiri481@gmail.com>
2025-05-03 20:54:30 +0545

    adding image attribute in the book table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,18 @@
 CREATE SCHEMA IF NOT EXISTS library_db;
 USE library_db;
 CREATE TABLE IF NOT EXISTS books (
-                                     book_id INT AUTO_INCREMENT PRIMARY KEY,
-                                     title VARCHAR(255) NOT NULL,
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
     publication_year DATE NOT NULL,
     category VARCHAR(100),
     stock INT DEFAULT 1
-
-    );
+    book_image MEDIUMBLOB,
+);
 
 CREATE TABLE IF NOT EXISTS users (
-                                     user_id INT AUTO_INCREMENT PRIMARY KEY,
-                                     full_name VARCHAR(100) NOT NULL,
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
     user_email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     confirmPassword VARCHAR(255) NOT NULL,
@@ -21,14 +21,15 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     );
+
 CREATE TABLE IF NOT EXISTS borrow (
-                                      borrow_id INT AUTO_INCREMENT PRIMARY KEY,
-                                      book_id INT NOT NULL,
-                                      user_id INT NOT NULL,
-                                      borrow_date DATE NOT NULL,
-                                      due_date DATE NOT NULL,
-                                      return_date DATE,
-                                      fine_amount DECIMAL(10, 2) DEFAULT 0.0,
+    borrow_id INT AUTO_INCREMENT PRIMARY KEY,
+    book_id INT NOT NULL,
+    user_id INT NOT NULL,
+    borrow_date DATE NOT NULL,
+    due_date DATE NOT NULL,
+    return_date DATE,
+    fine_amount DECIMAL(10, 2) DEFAULT 0.0,
     status ENUM('borrowed', 'returned', 'overdue', 'reserved') DEFAULT 'borrowed',
     FOREIGN KEY (book_id) REFERENCES books(book_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
@@ -36,11 +37,11 @@ CREATE TABLE IF NOT EXISTS borrow (
 
 
 CREATE TABLE IF NOT EXISTS reservations (
-                                            reservation_id INT AUTO_INCREMENT PRIMARY KEY,
-                                            book_id INT NOT NULL,
-                                            user_id INT NOT NULL,
-                                            reservation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                            status ENUM('active', 'expired', 'cancelled') DEFAULT 'active',
+    reservation_id INT AUTO_INCREMENT PRIMARY KEY,
+    book_id INT NOT NULL,
+    user_id INT NOT NULL,
+    reservation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('active', 'expired', 'cancelled') DEFAULT 'active',
     reserved_until DATE NOT NULL,
     FOREIGN KEY (book_id) REFERENCES books(book_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
